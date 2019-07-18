8f2c0d044743777af1170970a41684772fd5bd5a
Anne-Sophie Vandendooren <anne.vandendooren@gmail.com>
2019-07-18 17:33:06 +0200

    done

--- app.js
+++ app.js
@@ -2,31 +2,54 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+
 app.set("view engine", "hbs");
 
 const Schema = mongoose.Schema;
 
-const Restaurant = mongoose.model('restaurant', new Schema({
-    cuisine: String,
-    borough: String,
-    name: String,
-}), "restaurant");
+const Movie = mongoose.model('movies', new Schema({
+    title: String,
+    director: String,
+    stars: Array,
+    image: String,
+    description: String,
+    showtimes : Array,
+}), "movies");
 
-mongoose.connect('mongodb://localhost/foodapp', {useNewUrlParser: true}, (err)=> {
+mongoose.connect('mongodb://localhost/movie', {useNewUrlParser: true}, (err)=> {
     debugger
     if(err) console.log("ERROR EROROROR", err)
     else console.log("connected")
 });
 
-app.get("/restaurants",(req,res)=> {
-    let cuisine = req.query.cuisine
-    Restaurant.find({cuisine: cuisine})
-        .then((restaurants)=> {
-            res.render("restaurants.hbs", {restaurants: restaurants})
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/movies",(req,res)=> {
+    Movie.find({})
+        .then((movies)=> {
+            res.render("movies.hbs", {movies: movies})
+        })
+        .catch(err => {
+            console.log(err)
         })
 })
+app.get("/",(req,res)=> {
+    res.render("home.hbs") 
+})
+app.get("/movies",(req,res)=> {
+    res.render("movies.hbs") 
+})
 
-
+app.get("/movie",(req,res)=> {
+    const movieId = req.query.id;
+    Movie.findById(movieId, function(err, adventure){})
+        .then((movie)=> {
+            res.render("movie.hbs", {movie: movie})
+        })
+        .catch(err => {
+            console.log(err)
+})})
 
 app.listen(3000, ()=> {
     console.log("App listening")
